0ac768d3077ec15e1c13e597c0220e0b59146248
Yukineku <115874122+Yukineku@users.noreply.github.com>
2024-06-09 20:22:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,13 +2,97 @@
 #include <string>
 #include <map>
 #include "tstack.h"
-
+int Prior(char y) {
+  switch (y) {
+    case '(':
+      return 0;
+    case ')':
+      return 1;
+    case '+': case '-':
+      return 2;
+    case '*': case '/':
+      return 3;
+    default:
+      return -1;
+    }
+}
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+  std::string rez, rezult1;
+  TStack<char, 100>stack1;
+  for (auto& y : inf) {
+    int p = Prior(y);
+    if (p == -1) {
+      rez = rez + y + ' ';
+    } else {
+      char elem = stack1.get();
+      if (p == 0 || Prior(elem) < p || stack1.isEmpty()) {
+        stack1.push(y);
+      } else {
+        if (y == ')') {
+          while (Prior(elem) >= p) {
+            rez = rez + elem + ' ';
+            stack1.pop();
+            elem = stack1.get();
+          }
+          stack1.pop();
+        } else {
+          while (Prior(elem) >= p) {
+            rez = rez + elem + ' ';
+            stack1.pop();
+            elem = stack1.get();
+          }
+          stack1.push(y);
+        }
+      }
+    }
+  }
+  while (!stack1.isEmpty()) {
+    rez = rez + stack1.get() + ' ';
+    stack1.pop();
+  }
+  for (int i = 0; i < rez.size() - 1; i++)
+    rezult1 += rez[i];
+  return rezult1;
+}
+
+int schet(const int& p, const int& v, const int& y) {
+  switch (y) {
+    case '+':
+      return p + v;
+    case '-':
+      return p - v;
+    case '/':
+      return p / v;
+    case '*':
+      return p * v;
+    default:
+      return 0;
+  }
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+  TStack<int, 100> stack1;
+  std::string rez = "";
+  for (int i = 0; i < pref.size(); i++) {
+    char elem = pref[i];
+    if (Prior(elem) == -1) {
+      if (pref[i] == ' ') {
+        continue;
+      } else if (isdigit(pref[i+1])) {
+        rez += pref[i];
+        continue;
+      } else {
+        rez += pref[i];
+        stack1.push(atoi(rez.c_str()));
+        rez = "";
+      }
+    } else {
+      int v = stack1.get();
+      stack1.pop();
+      int p = stack1.get();
+      stack1.pop();
+      stack1.push(schet(p, v, elem));
+    }
+  }
+  return stack1.get();
 }
